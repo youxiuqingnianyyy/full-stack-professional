94a4867f613c6c92a6ecc220ce35a40438566d57
Yuyan Yang <yxqnyyy@gmail.com>
2023-11-30 16:03:12 +0100

    fix s3 test

--- backend/src/main/java/com/amigoscode/Main.java
+++ backend/src/main/java/com/amigoscode/Main.java
@@ -12,10 +12,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Random;
+import java.util.UUID;
 
 @SpringBootApplication
 public class Main {
@@ -27,16 +26,15 @@ public class Main {
     @Bean
     CommandLineRunner runner(
             CustomerRepository customerRepository,
-            PasswordEncoder passwordEncoder,
-            S3Service s3Service,
-            S3Buckets s3Buckets) {
+            PasswordEncoder passwordEncoder) {
         return args -> {
             createRandomCustomer(customerRepository, passwordEncoder);
-            //testBucketUploadAndDownload(s3Service, s3Buckets);
+            // testBucketUploadAndDownload(s3Service, s3Buckets);
         };
     }
 
-    private static void testBucketUploadAndDownload(S3Service s3Service, S3Buckets s3Buckets) {
+    private static void testBucketUploadAndDownload(S3Service s3Service,
+                                                    S3Buckets s3Buckets) {
         s3Service.putObject(
                 s3Buckets.getCustomer(),
                 "foo/bar/jamila",
@@ -70,14 +68,4 @@ public class Main {
         System.out.println(email);
     }
 
-    @RestController
-    public static class TestCDController {
-
-        record TestCD(String test){}
-
-        @GetMapping("/test")
-        public TestCD getTestCD() {
-            return new TestCD("Test");
-        }
-    }
 }
